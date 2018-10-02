85736406fea29dfb7bc070c75e8248e0cd8bbc31
ranitachi <fachran.nazarullah@gmail.com>
2018-10-02 10:23:01 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Radio Reksa Purwakarta : 100.9 FM</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -65,7 +65,10 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+        <center>
+            <img src="{{asset('images/staytuned.png')}}" style="height:650px;width:auto">
+        </center>
+        {{-- <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
@@ -90,6 +93,6 @@
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
-        </div>
+        </div> --}}
     </body>
 </html>
