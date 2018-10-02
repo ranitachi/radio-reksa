c48af4ab9e65b74e9b2caa764570a9105d75a7d4
ranitachi <fachran.nazarullah@gmail.com>
2018-10-02 23:45:53 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,10 @@
 |
 */
 
-// Route::get('/', function () {
-    //     return view('welcome');
-    // });
-Route::get('/', 'FrontController@index');
+Route::get('/', function () {
+        return view('welcome');
+    });
+Route::get('coba', 'FrontController@index');
 
 Auth::routes();
 
